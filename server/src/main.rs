080f83607a64146f815e5763fca4cde9356d9940
Maximillian Heth <info@edfx.co>
2025-04-01 12:19:26 -0500

    Add server

---
+++ server/src/main.rs
@@ -0,0 +1,13 @@
+use rocket::get;
+use rocket::launch;
+use rocket::routes;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
